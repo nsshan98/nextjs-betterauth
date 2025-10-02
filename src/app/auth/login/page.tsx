cadff3e46d66cccf60c5cffd254cc0da60af35f1
nsshan98 <n.sakibnss@gmail.com>
2025-10-02 22:45:21 +0600

    refactor: restructure login and signup components; integrate Radix UI elements

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,40 @@
-import { LoginComponent } from '@/components/login/login-component'
-import React from 'react'
+import { AppWindowIcon, CodeIcon } from "lucide-react";
 
-const Login = () => {
+import { Button } from "@/components/atoms/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/atoms/card";
+import { Input } from "@/components/atoms/input";
+import { Label } from "@/components/atoms/label";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/atoms/tabs";
+import LoginComponent from "@/components/auth/login-component";
+import SignupComponent from "@/components/auth/signup-component";
+
+export default function Login() {
   return (
-    <div className='flex justify-center items-center h-screen'><LoginComponent/></div>
-  )
+    <div className="mx-auto flex flex-col justify-center min-h-screen w-full max-w-md">
+      <Tabs defaultValue="login">
+        <TabsList>
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">Signup</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <LoginComponent />
+        </TabsContent>
+        <TabsContent value="signup">
+          <SignupComponent />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
-
-export default Login
---
+++ src/components/auth/login-component.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../atoms/card";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "../atoms/input";
+import { Button } from "../atoms/button";
+
+const LoginComponent = () => {
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>
+            Make changes to your account here. Click save when you&apos;re done.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-6">
+          <div className="grid gap-3">
+            <Label htmlFor="tabs-demo-name">Name</Label>
+            <Input id="tabs-demo-name" defaultValue="Pedro Duarte" />
+          </div>
+          <div className="grid gap-3">
+            <Label htmlFor="tabs-demo-username">Username</Label>
+            <Input id="tabs-demo-username" defaultValue="@peduarte" />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button>Save changes</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default LoginComponent;
