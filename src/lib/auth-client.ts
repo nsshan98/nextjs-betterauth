312e546222213e5ac20baa8050ca883e1b2e793c
nsshan98 <n.sakibnss@gmail.com>
2025-10-04 22:12:58 +0600

    feat: Add authentication client library with db setup

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,13 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:5000",
-})
+  /** The base URL of the server (optional if you're using the same domain) */
+  baseURL: "https://just-pd-backend-api-v1.onrender.com/api",
+
+  endpoints: {
+    signIn: {
+      email: "/login",
+    },
+  },
+});
+
+export const { signIn } = authClient
